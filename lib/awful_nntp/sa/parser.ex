82857d86774b9a9e80c0981496edff2cedf02e65
Andy Chan <github@sockbot.com>
2026-01-07 16:29:09 -0800

    Implement SA HTML parser
    - Add AwfulNntp.SA.Parser module with Floki
    - Implement parse_forum_list/1 - extracts forums with ID, name, description
    - Implement parse_thread_list/1 - extracts threads with title, author, replies
    - Implement parse_posts/1 - extracts post content
    - Update parser tests to match SA HTML structure
    - All 24 tests passing (24/24)
    - Successfully parses real SA HTML samples

---
+++ lib/awful_nntp/sa/parser.ex
@@ -0,0 +1,231 @@
+defmodule AwfulNntp.SA.Parser do
+  @moduledoc """
+  HTML parser for Something Awful forums.
+  Extracts forum lists, threads, and posts from SA HTML.
+  """
+
+  @doc """
+  Parses the forum list from the main forums page.
+  
+  Returns a list of forum maps with:
+  - id: Forum ID (string)
+  - name: Forum name
+  - description: Forum description (optional)
+  """
+  def parse_forum_list(html) do
+    case Floki.parse_document(html) do
+      {:ok, document} ->
+        forums =
+          document
+          |> Floki.find("tr.forum")
+          |> Enum.map(&extract_forum/1)
+          |> Enum.reject(&is_nil/1)
+
+        {:ok, forums}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  @doc """
+  Parses a forum's thread list.
+  
+  Returns a list of thread maps with:
+  - id: Thread ID (string)
+  - title: Thread title
+  - author: Username of thread author
+  - replies: Number of replies
+  - sticky: Boolean if sticky thread
+  - closed: Boolean if closed thread
+  """
+  def parse_thread_list(html) do
+    case Floki.parse_document(html) do
+      {:ok, document} ->
+        threads =
+          document
+          |> Floki.find("tr.thread")
+          |> Enum.map(&extract_thread/1)
+          |> Enum.reject(&is_nil/1)
+
+        {:ok, threads}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  @doc """
+  Parses a thread's posts.
+  
+  Returns a list of post maps with:
+  - id: Post ID (string)
+  - author: Username
+  - date: Post timestamp
+  - content: Post HTML content
+  """
+  def parse_posts(html) do
+    case Floki.parse_document(html) do
+      {:ok, document} ->
+        posts =
+          document
+          |> Floki.find("table.post")
+          |> Enum.map(&extract_post/1)
+          |> Enum.reject(&is_nil/1)
+
+        {:ok, posts}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  # Private helper functions
+
+  defp extract_forum(forum_row) do
+    # Extract forum ID from class attribute
+    forum_id =
+      forum_row
+      |> Floki.attribute("class")
+      |> List.first()
+      |> extract_forum_id()
+
+    # Extract forum name and URL
+    forum_link =
+      forum_row
+      |> Floki.find("td.title a.forum")
+      |> List.first()
+
+    case forum_link do
+      nil ->
+        nil
+
+      link ->
+        name = Floki.text(link)
+        href = Floki.attribute(link, "href") |> List.first()
+
+        # Extract description
+        description =
+          forum_row
+          |> Floki.find("span.forumdesc")
+          |> Floki.text()
+          |> String.trim()
+          |> String.trim_leading("-")
+          |> String.trim()
+
+        %{
+          id: forum_id,
+          name: name,
+          description: description,
+          href: href
+        }
+    end
+  end
+
+  defp extract_forum_id(class_string) when is_binary(class_string) do
+    case Regex.run(~r/forum_(\d+)/, class_string) do
+      [_, id] -> id
+      _ -> nil
+    end
+  end
+
+  defp extract_forum_id(_), do: nil
+
+  defp extract_thread(thread_row) do
+    # Extract thread ID from id attribute
+    thread_id =
+      thread_row
+      |> Floki.attribute("id")
+      |> List.first()
+      |> extract_thread_id()
+
+    # Extract title and link
+    title_link =
+      thread_row
+      |> Floki.find("a.thread_title")
+      |> List.first()
+
+    case title_link do
+      nil ->
+        nil
+
+      link ->
+        title = Floki.text(link)
+
+        # Extract author
+        author =
+          thread_row
+          |> Floki.find("td.author a")
+          |> Floki.text()
+
+        # Extract reply count
+        replies =
+          thread_row
+          |> Floki.find("td.replies a")
+          |> Floki.text()
+          |> String.to_integer()
+
+        # Check if sticky or closed
+        classes = Floki.attribute(thread_row, "class") |> List.first() || ""
+        sticky = String.contains?(classes, "sticky")
+        closed = String.contains?(classes, "closed")
+
+        %{
+          id: thread_id,
+          title: title,
+          author: author,
+          replies: replies,
+          sticky: sticky,
+          closed: closed
+        }
+    end
+  end
+
+  defp extract_thread_id("thread" <> id), do: id
+  defp extract_thread_id(_), do: nil
+
+  defp extract_post(post_table) do
+    # Extract post ID from id attribute
+    post_id =
+      post_table
+      |> Floki.attribute("id")
+      |> List.first()
+      |> extract_post_id()
+
+    # Extract author
+    author =
+      post_table
+      |> Floki.find("dt.author")
+      |> Floki.text()
+      |> String.trim()
+
+    # Extract date
+    date =
+      post_table
+      |> Floki.find("dd.postdate")
+      |> Floki.text()
+      |> String.trim()
+
+    # Extract post content
+    content =
+      post_table
+      |> Floki.find("td.postbody")
+      |> Floki.raw_html()
+
+    case post_id do
+      nil ->
+        nil
+
+      id ->
+        %{
+          id: id,
+          author: author,
+          date: date,
+          content: content
+        }
+    end
+  end
+
+  defp extract_post_id("post" <> id), do: id
+  defp extract_post_id(_), do: nil
+end
--- test/awful_nntp/sa/parser_test.exs
+++ test/awful_nntp/sa/parser_test.exs
@@ -7,19 +7,30 @@ defmodule AwfulNntp.SA.ParserTest do
     test "parses forum list from HTML" do
       html = """
       <html>
-        <div class="forum">
-          <a href="/forumdisplay.php?forumid=1">General Bullshit</a>
-        </div>
-        <div class="forum">
-          <a href="/forumdisplay.php?forumid=44">Games</a>
-        </div>
+        <tr class="forum forum_273">
+          <td class="icon">
+            <a href="forumdisplay.php?forumid=273"><img src="icon.gif" alt=""></a>
+          </td>
+          <td class="title">
+            <a class="forum" href="forumdisplay.php?forumid=273">General Bullshit</a>
+            <span class="forumdesc"> - This is the general discussion forum.</span>
+          </td>
+        </tr>
+        <tr class="forum forum_44">
+          <td class="icon">
+            <a href="forumdisplay.php?forumid=44"><img src="icon.gif" alt=""></a>
+          </td>
+          <td class="title">
+            <a class="forum" href="forumdisplay.php?forumid=44">Games</a>
+            <span class="forumdesc"> - Video games discussion.</span>
+          </td>
+        </tr>
       </html>
       """
 
-      # This will fail until we implement it - TDD!
       assert {:ok, forums} = Parser.parse_forum_list(html)
       assert length(forums) == 2
-      assert Enum.any?(forums, fn f -> f.name == "General Bullshit" && f.id == "1" end)
+      assert Enum.any?(forums, fn f -> f.name == "General Bullshit" && f.id == "273" end)
       assert Enum.any?(forums, fn f -> f.name == "Games" && f.id == "44" end)
     end
 
@@ -31,15 +42,45 @@ defmodule AwfulNntp.SA.ParserTest do
 
   describe "parse_thread_list/1" do
     test "parses thread list from HTML" do
-      # TODO: Add test once we define the HTML structure
-      assert true
+      html = """
+      <html>
+        <tr class="thread" id="thread12345">
+          <td class="title">
+            <a href="showthread.php?threadid=12345" class="thread_title">Test Thread</a>
+          </td>
+          <td class="author"><a href="">TestUser</a></td>
+          <td class="replies"><a href="">42</a></td>
+        </tr>
+      </html>
+      """
+
+      assert {:ok, threads} = Parser.parse_thread_list(html)
+      assert length(threads) == 1
+      thread = List.first(threads)
+      assert thread.id == "12345"
+      assert thread.title == "Test Thread"
+      assert thread.author == "TestUser"
+      assert thread.replies == 42
     end
   end
 
-  describe "parse_post/1" do
-    test "parses post content from HTML" do
-      # TODO: Add test once we define the HTML structure
-      assert true
+  describe "parse_posts/1" do
+    test "parses posts from thread" do
+      html = """
+      <html>
+        <table class="post" id="post67890">
+          <dt class="author">PostAuthor</dt>
+          <dd class="postdate">Jan 01, 2026</dd>
+          <td class="postbody">Post content here</td>
+        </table>
+      </html>
+      """
+
+      assert {:ok, posts} = Parser.parse_posts(html)
+      assert length(posts) == 1
+      post = List.first(posts)
+      assert post.id == "67890"
+      assert post.author == "PostAuthor"
     end
   end
 end
